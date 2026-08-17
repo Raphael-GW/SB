8b569f6f78a22fbf118e87aa128dfb4fe1d48b48
Gaias <gaias.raphael@ufpr.br>
2026-08-17 15:13:34 -0300

    feito aula 4

---
+++ GRR20244362/Aula4/exercicio1.s
@@ -0,0 +1,13 @@
+.text
+.global main
+.type main, @function
+main:
+	pushq %rsp #salva o frame pointer na pilha
+	movq %rsp, %rbp #copiar o stack pointer para o frame pointer
+	subq $16, %rsp #abre 16 bytes de espaço na pilha
+	movl $1, 12(%rsp)
+	movl $0, 8(%rsp)
+	movl $0, %eax
+	movq %rbp, %rsp
+	popq %rbp
+	ret
---
+++ GRR20244362/Aula4/exercicio2.s
@@ -0,0 +1,14 @@
+.text
+.global main
+.type main, @function
+main:
+	pushq %rsp #salva o frame pointer na pilha
+	movq %rsp, %rbp #copiar o stack pointer para o frame pointer
+	subq $16, %rsp #abre 16 bytes de espaço na pilha
+	movl $1, 12(%rsp)
+	movl $0, 8(%rsp)
+	movl $5, %eax
+	int3 #interrompe o programa
+	movq %rbp, %rsp
+	popq %rbp
+	ret
