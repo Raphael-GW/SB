11c3b78337a58f23da020b299916fcd0a35aa4d8
Raphael-GW <gaias.raphael@ufpr.br>
2026-08-21 10:41:57 -0300

    terminar ex 3 aula5

---
+++ GRR20244362/Aula5/Exercicio3/somaChar.s
@@ -0,0 +1,15 @@
+.equ STDIN, 0
+.equ STDOUT, 1
+
+.equ localSize, 16
+
+.text
+.global main
+.type main, @function
+main:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $localSize, %rsp
+
+    movl $3, %edx
+    leaq
