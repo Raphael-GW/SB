b69d708a975564af00a1241cc98461d5707961c8
Raphael-GW <gaias.raphael@ufpr.br>
2026-08-27 17:19:55 -0300

    Refactor minfib.s for variable initialization and format

--- GRR20244362/Aula7/Exercicio4/minfib.s
+++ GRR20244362/Aula7/Exercicio4/minfib.s
@@ -1,10 +1,12 @@
 .equ localSize, -16
-.equ ptrInt, -8
+.equ ptrInt, -4
+.equ varA, -8
+.equ varB, -12
+.equ varSoma, -16
 
-.section .rodata
+section .rodata
 scanFormat:
     .string "%d"
-
 .text
 .globl main
 .type main, @function
@@ -12,11 +14,23 @@ main:
     pushq %rbp
     movq %rsp, %rbp
     addq $localSize, %rsp
-
+    
     leaq ptrInt(%rbp), %rsi
     movq $scanFormat, %rdi
     movq $0, %rax
     call scanf
-    leaq ptrInt(%rbp), %rbx  # 
-fibonacci:
     
+    #inicia as variaveis
+    movq $0, %r8  # a
+    movq $1, %r9  # b
+    movq $0, %r10 # soma
+    
+    leaq ptrInt(%rbp), %rdx
+    cmpq %
+while:
+    cmpq %
+printb:
+    leaq %r9, %rsi
+    movq $scanFormat, %rdi
+    movq $0, %rax
+    call printf
