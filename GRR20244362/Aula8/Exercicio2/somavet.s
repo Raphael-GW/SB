d91bb0554b5e1e9414fb62822e2000fcb38199e6
Raphael-GW <gaias.raphael@ufpr.br>
2026-08-31 11:59:01 -0300

    terminar somavet

--- GRR20244362/Aula8/Exercicio2/somavet.s
+++ GRR20244362/Aula8/Exercicio2/somavet.s
@@ -1,4 +1,5 @@
 .equ STDOUT, 1
+.equ localSize, 80
 
 .section .bss
 .lcomm Vet1 64
@@ -8,4 +9,28 @@
 .globl main
 .type main, @function
 main:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $localSize, %rsp
+
+    movq $Vet1, %rcx
+    movl $64, %eax
+loop_vet1:
+    movl $64, %rdx
+    subl %eax, %edx
+    movb %dl, (%rcx)
+    incq %rcx
+    subl $1, %eax
+    jnz loop_vet1
+
+    movq $Vet2, %rcx
+    movl $64, %eax
+loop_vet2:
+    movl $eax, %edx
+    movb %dl, (%rcx)
+    incq %rcx
+    subl $1, %eax
+    jnz loop_vet2
+
+
     
